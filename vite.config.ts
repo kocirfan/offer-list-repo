c3124a3c70b79610883e28e78fd79b57771bed98
İrfan Koçaksoy <irfnk83@gmail.com>
2025-05-22 14:02:12 +0300

    son5

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import json from '@rollup/plugin-json';
+
 
 installGlobals({ nativeFetch: true });
 
@@ -63,6 +65,7 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    json()
   ],
   build: {
     assetsInlineLimit: 0,
